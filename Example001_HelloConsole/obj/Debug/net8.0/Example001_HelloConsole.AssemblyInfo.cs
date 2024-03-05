57499e10aae96d8f72a24f6df49b411fbec982e2
TatsianaMishkova <Tanysha0292@gmail.com>
2024-03-05 14:24:55 +0300

    add Random project

--- Example001_HelloConsole/obj/Debug/net8.0/Example001_HelloConsole.AssemblyInfo.cs
+++ Example001_HelloConsole/obj/Debug/net8.0/Example001_HelloConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Example001_HelloConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57730bd03cc383c30950d8fb5a6dfef4367cde4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f3a2ea3ce16f003776c1ca3c6cc991541215f25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Example001_HelloConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Example001_HelloConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
