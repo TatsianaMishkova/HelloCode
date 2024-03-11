897d3c3ca9ed2b780b03396c8120c486499b89d2
TatsianaMishkova <Tanysha0292@gmail.com>
2024-03-11 14:35:38 +0300

    add Example008

--- Example001_HelloConsole/obj/Debug/net8.0/Example001_HelloConsole.AssemblyInfo.cs
+++ Example001_HelloConsole/obj/Debug/net8.0/Example001_HelloConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Example001_HelloConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f3a2ea3ce16f003776c1ca3c6cc991541215f25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e75bd47b47a03222e6c13bbe96417d69bec1bce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Example001_HelloConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Example001_HelloConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 12, 64, 93, 26, 59, 82, 82};
+
+int n = array.Length;
+int find = 82;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
